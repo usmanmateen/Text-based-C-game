0cab7ff078790c0c74d112829143d0ea84bd9c1f
usman <usmanmateen255@gmail.com>
2023-02-13 20:19:01 +0000

    Fixed all bugs

--- Map.cpp
+++ Map.cpp
@@ -5,23 +5,47 @@
 
 using namespace std;
 
+string hallwayDescriptions[5]={"A dark spooky hallway with cobwebs hanging from the chandlier and ceiling.",
+                                "A wide and well lit hallway with mysterious symbols etched in the walls.",
+                                "A narrow and maze kind hallway with strange noises.",
+                                "A dim lit narrow hallway with cobwebs hanging from the lights.",
+                                "Wide and empty hallway with spiders and snakes crawling."};
+
+string forestDescriptions[5]= {"A dark and spooky forest with sense of danger lurking around every corner.",
+                                "A magical forest with dense trees and and not light coming through.",
+                                "A mystical forest with dense trees and creatures beyond imagination lurking in them.",
+                                "A forest where you can hear birds chipring and spring fountains.",
+                                "A spooky night in the forest where you can only hear warewolves."};
+        
+string roomDescriptions[5] = {"A dimly lit room with candles and mysterious symbols etched into the walls.", 
+                              "A grand room with intricate carvings and ornate furnishings with high ceilings.", 
+                              "A spacious room filled with beautiful artwork and elegant furnishings.",
+                              "A rustic room with natural wood accents and a stone fireplace, creating a peaceful and inviting atmosphere.",
+                              "A big room with high ceilings and chandliers with a fireplace "};
+
+string patioDescriptions[4] = {"A peaceful outdoor sitting area with a grill and water slides but everything looks spooky and cold.", 
+                                "A grand outdoor terrace with a magnificent view of mountains with clouds covering their tips you can hear warewolfs howling afar.", 
+                                "A terrace like out of the books of Harry Potter so cold like Dementors have been here and no happiness is left behind",
+                                "A field which has no end and looks spooky like a haunted graveyard with Zoomibies"};
+
+string townSquareDescriptions[5]= {"The centre of the town with a wrecked fountain and broken into shops.", 
+                                    "An abandoned town square with wind gusts and darkness.",
+                                    "A town quare with spooky shops and back alleys.",
+                                    "A strange looking alley heading towards town square with darkness.",
+                                    "Centre of the town with an abandonded car and multiple vans."};
+
 
 
 class Hallway {
     private:
         string name;
         string hDescription;
-        string hallwayDescriptions[5]={"A dark spooky hallway with cobwebs hanging from the chandlier and ceiling.",
-                                       "A wide and well lit hallway with mysterious symbols etched in the walls.",
-                                       "A narrow and maze kind hallway with strange noises.",
-                                       "A dim lit narrow hallway with cobwebs hanging from the lights.",
-                                        "Wide and empty hallway with spiders and snakes crawling."};
-
+       
     public:
-        Hallway(string n) : name(n){
+        Hallway(string n) {
+            this->name = n;
             srand(time(0));
-            int randomHallwayDescriptions= rand()%5;
-            hDescription = hallwayDescriptions[randomHallwayDescriptions];
+            hDescription = hallwayDescriptions[rand()%5];
         }
 
         string getName() {
@@ -33,12 +57,7 @@ class Hallway {
         }
 };
 
-string forestDescriptions[5]= {"A dark and spooky forest with sense of danger lurking around every corner.",
-                                        "A magical forest with dense trees and and not light coming through.",
-                                        "A mystical forest with dense trees and creatures beyond imagination lurking in them.",
-                                        "A forest where you can hear birds chipring and spring fountains.",
-                                        "A spooky night in the forest where you can only hear warewolves."};
-        
+
 class Forest {
     private:
         string name;
@@ -65,18 +84,15 @@ class Room {
     private:
         string name;
         string rDescription;
-        string roomDescriptions[5] = {"A dimly lit room with candles and mysterious symbols etched into the walls.", 
-                                      "A grand room with intricate carvings and ornate furnishings with high ceilings.", 
-                                      "A spacious room filled with beautiful artwork and elegant furnishings.",
-                                      "A rustic room with natural wood accents and a stone fireplace, creating a peaceful and inviting atmosphere.",
-                                      "A big room with high ceilings and chandliers with a fireplace "};
+
 
 
     public:
-        Room(string n) : name(n){
+        Room(string n) {
+            this->name = n;
             srand(time(0));
-            int randomRoomDescriptions = rand() % 5;
-            rDescription = roomDescriptions[randomRoomDescriptions];
+            
+            rDescription = roomDescriptions[rand() % 5];
         }
 
         string getName() {
@@ -92,21 +108,17 @@ class Patio {
     private:
         string name;
         string pDescription;
-        string patioDescriptions[4] = {"A peaceful outdoor sitting area with a grill and water slides but everything looks spooky and cold.", 
-                                       "A grand outdoor terrace with a magnificent view of mountains with clouds covering their tips you can hear warewolfs howling afar.", 
-                                       "A terrace like out of the books of Harry Potter so cold like Dementors have been here and no happiness is left behind",
-                                       "A field which has no end and looks spooky like a haunted graveyard with Zoomibies"};
-
-
+        
 
 
 
 
     public:
-        Patio(string n) : name(n){
+        Patio(string n) {
+            this->name = n;
             srand(time(0));
-            int randomPatioDescriptions = rand() % 4;
-            pDescription = patioDescriptions[randomPatioDescriptions]; 
+
+            pDescription = patioDescriptions[rand() % 4]; 
         }
 
         string getName() {
@@ -122,18 +134,14 @@ class TownSquare {
     private:
         string name;
         string tDescription;
-        string townSquareDescriptions[5]= {"The centre of the town with a wrecked fountain and broken into shops.", 
-                                            "An abandoned town square with wind gusts and darkness.",
-                                            "A town quare with spooky shops and back alleys.",
-                                            "A strange looking alley heading towards town square with darkness.",
-                                            "Centre of the town with an abandonded car and multiple vans."};
+        
 
 
     public:
-        TownSquare(string n) : name(n){
+        TownSquare(string n) {
+            this-> name = n;
             srand(time(0));
-            int randomTownSquareDescriptions = rand() % 5;
-            tDescription=townSquareDescriptions[randomTownSquareDescriptions];
+            tDescription=townSquareDescriptions[rand() % 5];
         }
 
         string getName() {
@@ -147,11 +155,11 @@ class TownSquare {
 
 
 int main() {
-    Room room1;
-    Hallway hallway1;
+    Room room1("Mysterious Room");
+    Hallway hallway1("Spooky Park");
     Forest forest1 ( "Wailing woods");
-    Patio patio1;
-    TownSquare townSquare;
+    Patio patio1("Cold open space");
+    TownSquare townSquare("Town Square");
 
     cout << room1.getName() << ": " << room1.getDescription() << endl;
     cout << hallway1.getName() << ": " << hallway1.getDescription() << endl;
