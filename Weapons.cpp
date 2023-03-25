d557232db265a256b9f0d3400da830a08492cace
usman <usmanmateen255@gmail.com>
2023-03-25 02:49:40 +0000

    Added reload and fire feature for weapons and quit

--- Weapons.cpp
+++ Weapons.cpp
@@ -18,20 +18,44 @@ class weapon : public backpack {
     public:
         int damage;
         int range;
+        int bullets; // added bullets member variable
         string type;
 
-        
         weapon() {
             damage = 0;
             range = 0;
             type = "Unknown";
+            bullets = 0; // initialize bullets to zero
         }
-        weapon(int d, int r, string t) {
+        weapon(int d, int r, int b, string t) { // added bullets argument
             damage = d;
             range = r;
             type = t;
+            bullets = b; // set the initial number of bullets
         }
+
+        
+
+        void reload(int numBullets) {
+            if (numBullets > 40) {
+                numBullets = 40;
+            }
+        cout << "Changing to a fresh Mag for " << type << " with " << numBullets << endl;
+        bullets += numBullets;
+
+        }
+        void fire() {
+            if (bullets > 0) {
+                bullets--;
+                cout << "Bang! Bullets left: " << bullets << endl;
+            } else {
+                cout << "Click! Out of bullets." << endl;
+                
+    }
+}
+
 };
+
 class healing : public backpack {
     public:
         int health;
@@ -63,22 +87,7 @@ char switchItem(vector<weapon>& weapons, vector<healing>& healings){
     return choice;
 
 }
-
-int selectWeapon(vector<weapon>& weapons) {
-    cout << "Weapons in bagpack:" << endl;
-    for (int i = 0; i < weapons.size(); ++i ) {
-        cout << i+1 << ". " << weapons[i].type << endl;
-    }
-    int choice;
-    cout << "Enter the number of the weapon you wish to choose: ";
-    cin >> choice;
-    while (choice < 1 || choice > weapons.size()) {
-        cout << "Void choice. Please enter a number between 1 and " << weapons.size() << ": ";
-        cin >> choice;
-    }
-    return choice-1;
-}
-int selectHealing(vector<healing>& healings) {
+int selectHealing(vector<healing>& healings){
     cout << "Healing items in backpack:" << endl;
     for (int i = 0; i < healings.size(); ++i ) {
         cout << i+1 << ". " << healings[i].type << endl;
@@ -87,26 +96,112 @@ int selectHealing(vector<healing>& healings) {
     cout << "Enter the number of the healing item you wish to use: ";
     cin >> choice;
 
+    cin.clear();
+    cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+
     while (choice < 1 || choice > healings.size()) {
-        cout << "Void choice. Please enter a number between 1 and " << healings.size() << ": ";
+        if (cin.fail()) {
+            cin.clear();
+            cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+            cout << "Invalid choice. Please enter a number between 1 and " << healings.size() << ": ";
+            return -1;
+        }
+        cout << "Invalid choice. Please enter a number between 1 and " << healings.size() << ": ";
+        cin >> choice;
+    }
+    return choice-1;
+}
+
+int selectWeapon(vector<weapon>& weapons, vector<healing>& healings) {
+    while (true) {
+        cout << "Weapons in backpack:" << endl;
+        for (int i = 0; i < weapons.size(); ++i ) {
+            cout << i+1 << ". " << weapons[i].type << " - Bullets: " << weapons[i].bullets << endl;
+        }
+        int choice;
+        cout << "Enter the number of the weapon you wish to choose, enter 'h' to switch to healing items, or enter 'q' to quit: ";
+        cin >> choice;
+        if (cin.fail()) {
+            cin.clear();
+            cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+            cout << "Invalid choice. Please enter a number between 1 and " << weapons.size() << ", 'h' to switch to healing items, or 'q' to quit: ";
+        } else if (choice == 'q') {
+            cout << "Quitting weapon selection." << endl;
+            return -1;
+        } else if (choice == 'h') {
+            return -2;
+        } else if (choice < 1 || choice > weapons.size()) {
+            cout << "Invalid choice. Please enter a number between 1 and " << weapons.size() << ", 'h' to switch to healing items, or 'q' to quit: ";
+        } else {
+            choice--; // subtract 1 to get the index of the selected weapon
+            weapon selectedWeapon = weapons[choice];
+            cout << "You have selected the " << selectedWeapon.type << "." << endl;
+
+            while (true) {
+                cout << "Enter 'f' to fire or 'r' to reload, or enter 'h' to switch to healing items, or enter 'q' to quit: ";
+                char action;
+                cin >> action;
+                if (action == 'q') {
+                    cout << "Quitting weapon action." << endl;
+                    return -1;
+                } else if (action == 'h') {
+                    return -2;
+                } else if (action != 'f' && action != 'r') {
+                    cout << "No such action in directory. Please enter 'f' to fire or 'r' to reload, or enter 'h' to switch to healing items, or enter 'q' to quit: ";
+                } else {
+                    if (action == 'r') {
+                        int numBullets;
+                        cout << "Mag Size ";
+                        cin >> numBullets;
+                        selectedWeapon.reload(numBullets);
+                    } else {
+                        selectedWeapon.fire();
+                    }
+                }
+            }
+        }
+    }
+}
+
+
+
+int selectHealing(vector<healing>& healings){
+    cout << "Healing items in backpack:" << endl;
+    for (int i = 0; i < healings.size(); ++i ) {
+        cout << i+1 << ". " << healings[i].type << endl;
+    }
+    int choice;
+    cout << "Enter the number of the healing item you wish to use: ";
+    cin >> choice;
+
+    cin.clear();
+    cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+
+    while (choice < 1 || choice > healings.size()) {
+        if (cin.fail()) {
+            cin.clear();
+            cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+            cout << "Invalid choice. Please enter a number between 1 and " << healings.size() << ": ";
+            return -1;
+        }
+        cout << "Invalid choice. Please enter a number between 1 and " << healings.size() << ": ";
         cin >> choice;
     }
     return choice-1;
 }
 
 
-
 int main() {
 
     vector<weapon> weapons;
-    weapons.push_back(weapon(20, 2, "Sword"));
-    weapons.push_back(weapon(60, 10, "Shotgun"));
-    weapons.push_back(weapon(30, 15, "SMG"));
-    weapons.push_back(weapon(10, 1, "Melee"));
-    weapons.push_back(weapon(50, 20, "TAQ-56"));
-    weapons.push_back(weapon(40, 50, "M4"));
-    weapons.push_back(weapon(40, 30, "Pistol"));
-    weapons.push_back(weapon(100, 100, "Heavy sniper"));
+    weapons.push_back(weapon(20, 2,10, "Sword"));
+    weapons.push_back(weapon(60, 10,5, "Shotgun"));
+    weapons.push_back(weapon(30, 15, 45, "SMG"));
+    weapons.push_back(weapon(10, 1, 10, "Melee"));
+    weapons.push_back(weapon(50, 20,30, "TAQ-56"));
+    weapons.push_back(weapon(40, 50,60,  "M4"));
+    weapons.push_back(weapon(40, 30,15, "Pistol"));
+    weapons.push_back(weapon(100, 100,8, "Heavy sniper"));
 
 
     vector<healing> healings;
@@ -122,14 +217,17 @@ int main() {
     
     
     if (choice == 'w') {
-        int itemIndex = selectWeapon(weapons);
+        itemIndex = selectWeapon(weapons , healings);
         weapon chosenWeapon = weapons[itemIndex];
         cout << "You have switched to the " << chosenWeapon.type << "!" << endl;
         cout << "Damage: " << chosenWeapon.damage << endl;
         cout << "Range: " << chosenWeapon.range << endl;
+    
+        chosenWeapon.fire();
+        }
         
-    } else {
-        int itemIndex = selectHealing(healings);
+      else {
+        itemIndex = selectHealing(healings);
         healing chosenHealing = healings[itemIndex];
         cout << "You have switched to the " << chosenHealing.type << "!" << endl;
         cout << "Restores: " << chosenHealing.health << " health" << endl;
